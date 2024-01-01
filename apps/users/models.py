8ed7140c931ca2da4e86a0fcb6ad1256ff031849
Akumbom Wesley <94924532+AkumbomWesley@users.noreply.github.com>
2024-01-01 18:05:23 +0100

    (#2) - Implement User Authentication (#4)
    * Implement User Authentication
    
    * Add package

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from apps.core.models import BaseModel
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        user = self.create_user(email, first_name, last_name, password=password)
+        user.role = 'admin'
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(BaseModel, AbstractBaseUser):
+    ROLE_CHOICES = [
+        ('owner', 'Store Owner'),
+        ('staff', 'Store Staff'),
+        ('customer', 'Customer'),
+        ('admin', 'admin')
+    ]
+
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    ]
+
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    gender = models.CharField(max_length=16, choices=GENDER_CHOICES)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    address = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+    class Meta:
+        verbose_name = 'User Account'
+        verbose_name_plural = 'User Accounts'
