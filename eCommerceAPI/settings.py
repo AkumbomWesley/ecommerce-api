30f68d923efc1effc0675e1c4f7b2204df282101
AkumbomWesley <keyzwesley@gmail.com>
2024-03-22 04:55:59 +0100

    Added packages and changed database type

--- eCommerceAPI/settings.py
+++ eCommerceAPI/settings.py
@@ -123,20 +123,12 @@ SIMPLE_JWT = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+DDATABASES = {
     'default': {
-     'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'ecommerceapi',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'STRICT_ALL_TABLES',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
